37fa048b68643efe839c083dcd912d9af0c406cb
Antoniogtzv1905 <121040593@upq.edu.mx>
2025-06-12 18:36:59 -0600

    Practica 6 styleflex

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,35 +1,50 @@
 /* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
-import { Children } from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
-import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
 
-const Texto=()=>{
-  const[contenido, setContenido]=useState('hola mundo React')
-  const actualizarTexto=()=>{setContenido('estado actualizado')}
-  return(<Text onPress={actualizarTexto}>{contenido}</Text>) 
-}
+/* Componente personalizado */
+const Texto = ({ style }) => {
+  const [contenido, setContenido] = useState('hola mundo React');
+  const actualizarTexto = () => {
+    setContenido('estado actualizado');
+  };
+  return (
+    <Text style={[styles.text, style]} onPress={actualizarTexto}>
+      {contenido}
+    </Text>
+  );
+};
 
-/* zona 2 , el main*/
+/* zona 2 , el main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Texto>  </Texto>
-      <Texto> </Texto>
-      <Texto> </Texto>
-      <Button title='Presioname!'></Button>
+      <Texto style={styles.red} />
+      <Texto style={styles.blue} />
+      <Texto style={styles.green} />
+      <Button title="Presioname!" onPress={() => {}} />
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
-/* zona 3, estilos */ 
+/* zona 3, estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'space-evenly',
+    flexDirection:'colum',
   },
+  text: {
+    color: 'black',
+    fontSize: 25,
+    height: 100,
+    width: 100,
+  },
+  red:  {backgroundColor: 'red' },
+  blue: {backgroundColor: 'blue' },
+  green:{backgroundColor: 'green' },
 });
